e247a0fc460ab61c6731cbea27a08ce6f147ed21
Suejung Shin <suejung.shin@sentry.io>
2024-08-15 21:10:38 -0700

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { codecovVitePlugin } from "@codecov/vite-plugin";
 
 export default defineConfig({
   build: {
